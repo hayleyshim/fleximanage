866dd96dbe9e06c68f64a0bc97755836b4710b8b
Omeraha <omerah@flexiwan.com>
2020-01-12 13:51:50 +0000

    Update DevSwVersionUpdateManager.js

--- backend/deviceLogic/DevSwVersionUpdateManager.js
+++ backend/deviceLogic/DevSwVersionUpdateManager.js
@@ -1,5 +1,4 @@
-// flexiWAN SD-WAN software - flexiEdge, flexiManage.
-// For more information go to https://flexiwan.com
+// flexiWAN SD-WAN software - flexiEdge, flexiManage. For more information go to https://flexiwan.com and even some more data...
 // Copyright (C) 2019  flexiWAN Ltd.
 
 // This program is free software: you can redistribute it and/or modify
